c09d6a8be8a38db1e02963bc9c7b17306eaba291
vromerov <vromerov@gmail.com>
2016-02-27 19:06:09 -0600

    address form with geo

--- pubweb/application/language/spanish/registration_lang.php
+++ pubweb/application/language/spanish/registration_lang.php
@@ -2,4 +2,22 @@
 $lang['user_registration_headline'] = "Para poder agilizar tu pedido por favor proporciona tus datos";
 $lang['user_registration_firstname'] = "Nombre";
 $lang['user_registration_lastname'] = "Apellidos";
+
+
+
+
+$lang['user_address_street'] = "Calle";
+$lang['user_addess_num_ext'] = "Número Exterior";
+$lang['user_addess_num_int'] = "Número Interior";
+$lang['user_addess_postal_code'] = "Código Postal";
+$lang['user_addess_neighborhood'] = "Colonia";
+$lang['user_addess_municipio'] = "Municipio";
+$lang['user_addess_state'] = "Estado";
+$lang['user_addess_phone'] = "Teléfono";
+$lang['user_addess_state'] = "Estado";
+$lang['user_addess_cellPhone'] = "Teléfono Celular";
+$lang['user_addess_marker'] = "Tú estás aqui";
+
+
+
 ?>
--- pubweb/application/views/address_registration.php
+++ pubweb/application/views/address_registration.php
@@ -67,6 +67,9 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 		width: 500px;
 		height: 400px;
 	  }
+	  label{ 
+	  	display:block;
+	  }
 	</style>
 
 </head>
@@ -76,41 +79,47 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 	<h1>Welcome to CodeIgniter!</h1>
 
 	<div id="body">
-		
-
 		<div id="map"></div>
-		<?=form_open('email/send')?>
-			<?php echo form_label(lang("user_registration_firstname"),"street");?>
-			<?php echo form_input("street","");?>
-			<?php echo form_label(lang("user_registration_lastname"),"num_ext");?>
-			<?php echo form_input("num_ext","");?>        
-			<?php echo form_label(lang("user_registration_birthdate"),"num_int");?>
-			<?php echo form_input("num_int","");?>
-			<?php echo form_label(lang("user_registration_birthdate"),"postalCode");?>
-			<?php echo form_input("postal_code","");?>
-			<?php echo form_label(lang("user_registration_birthdate"),"colonia");?>
-			<?php echo form_input("colonia","");?>
-			<?php echo form_label(lang("user_registration_birthdate"),"municipio");?>
-			<?php echo form_input("municipio","");?>
-			<?php echo form_label(lang("user_registration_birthdate"),"state");?>
-			<?php echo form_input("state","");?>
-			<?php echo form_label(lang("user_registration_birthdate"),"phone");?>
-			<?php echo form_input("phone","");?>
-			<?php echo form_label(lang("user_registration_birthdate"),"cellPhone");?>
-			<?php echo form_input("cellPhone","");?>
-		<?=form_close()?>
-
-
+		<div>
+			<?=form_open('email/send')?>
+				<?php echo form_label(lang("user_address_street"),"street");?>
+				<?php echo form_input("street","", ["id" => "street"]);?>
+				<?php echo form_label(lang("user_addess_num_ext"),"num_ext");?>
+				<?php echo form_input("num_ext","", ["id" => "num_ext"]);?>        
+				<?php echo form_label(lang("user_addess_num_int"),"num_int");?>
+				<?php echo form_input("num_int","", ["id" => "num_int"]);?>
+				<?php echo form_label(lang("user_addess_postal_code"),"postalCode");?>
+				<?php echo form_input("postalCode","", ["id" => "postalCode"]);?>
+				<?php echo form_label(lang("user_addess_neighborhood"),"neighborhood");?>
+				<?php echo form_input("neighborhood","", ["id" => "neighborhood"]);?>
+				<?php echo form_label(lang("user_addess_municipio"),"municipio");?>
+				<?php echo form_input("municipio","", ["id" => "municipio"]);?>
+				<?php echo form_label(lang("user_addess_state"),"state");?>
+				<?php echo form_input("state","", ["id" => "state"]);?>
+				<?php echo form_label(lang("user_addess_phone"),"phone");?>
+				<?php echo form_input("phone","", ["id" => "phone"]);?>
+				<?php echo form_label(lang("user_addess_cellPhone"),"cellPhone");?>
+				<?php echo form_input("cellPhone","", ["id" => "cellPhone"]);?>
+			<?=form_close()?>
+		</div>
 	</div>
 
 	<p class="footer">Page rendered in <strong>{elapsed_time}</strong> seconds. <?php echo  (ENVIRONMENT === 'development') ?  'CodeIgniter Version <strong>' . CI_VERSION . '</strong>' : '' ?></p>
 </div>
+
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.0/jquery.min.js"></script>
 <script type="text/javascript" src="http://maps.google.com/maps/api/js?sensor=true"></script>
+
+
 	<article>
 
 	</article>
 <script>
+
+//var geocoder = new google.maps.Geocoder();
+
 function success(position) {
+
   var mapcanvas = document.createElement('div');
   mapcanvas.id = 'map';
   mapcanvas.style.height = '400px';
@@ -135,9 +144,47 @@ function success(position) {
   var marker = new google.maps.Marker({
 	  position: coords,
 	  map: map,
-	  title:"You are here!"
+	  draggable: true,
+	  title:'<?=lang("user_addess_marker")?>'
   });
+
+
+  /*google.maps.event.addListener(marker, 'dragend', function() {
+    //updateMarkerStatus('Drag ended');
+    geocodePosition(marker.getPosition());
+  });*/
+
+  var dataAddress = {
+  	"street" : "Montes Urales",
+  	"num_int": "",
+  	"num_ext": "445",
+  	"postalCode": "11000",
+  	"neighborhood": " Lomas de Chapultepec",
+  	"municipio": "Miguel Hidalgo",
+  	"state" : "Ciudad de México"
+  };
+
+  $.each(dataAddress, function (key, value) {
+  		$("#"+key).val(value);
+  });
+
 }
+/*
+function geocodePosition(pos) {
+  geocoder.geocode({
+    latLng: pos
+  }, function(responses) {
+    if (responses && responses.length > 0) {
+    	console.log(responses);
+    	console.log(responses[0].formatted_address)
+      //updateMarkerAddress(responses[0].formatted_address);
+    } else {
+    	alert("no se pudo");
+      //updateMarkerAddress('Cannot determine address at this location.');
+    }
+  });
+}*/
+
 
 if (navigator.geolocation) {
   navigator.geolocation.getCurrentPosition(success);
